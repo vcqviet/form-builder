4bedaa49da7b9a137d55b691da8a19de6da64387
viet vom <vcqviet@gmail.com>
2020-09-07 07:34:10 +0700

    build

---
+++ app/build/precache-manifest.7c62d442f92620ccb27254bad029976a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b2b14de8f05c01980423e3716b9ed2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1ff69d1ceb6a043775",
+    "url": "/static/css/main.03e821c2.chunk.css"
+  },
+  {
+    "revision": "1cc4db16e54eae0399bc",
+    "url": "/static/js/2.0e0f5722.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0e0f5722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1ff69d1ceb6a043775",
+    "url": "/static/js/main.10958518.chunk.js"
+  },
+  {
+    "revision": "a65715e6cc359fae963a",
+    "url": "/static/js/runtime-main.9150a93f.js"
+  }
+]);
